cec631d05ccf3302f33f9eb8ca9381acf1aafa4a
pongkaming@gmail.com <pongkaming@gmail.com@fa44827f-90ed-1370-a949-3c9141fc32c1>
2011-02-07 16:53:09 +0000

    Add the Javadoc for Poi

--- trunk/src/edu/cuhk/cubt/bus/Poi.java
+++ trunk/src/edu/cuhk/cubt/bus/Poi.java
@@ -18,6 +18,13 @@ public class Poi{
 	
 	protected static final String LOCATION_PROVIDER = "CUBT_POI";
 	
+	/**
+	 * 
+	 * @param name
+	 * @param latitude
+	 * @param longitude
+	 * @param range
+	 */
 	public Poi(String name, double latitude, double longitude, float range){
 		location = new Location(LOCATION_PROVIDER);
 		location.setLatitude(latitude);
@@ -65,7 +72,27 @@ public class Poi{
 		return location.distanceTo(dest) <= this.range;
 	}
 	
+	
+	/**
+	 * Returns the Poi that represent the input Location dest,
+	 * return null if no Poi matched
+	 * @param dest the Location to be check
+	 * @return the Poi that represent the input Location dest,
+	 * return null if no Poi matched
+	 */
 	public static Poi getByLocation(Location dest){
+		//TODO
+		return null;
+	}
+	
+	/**
+	 * Returns the Poi that represent the input name,
+	 * return null if no Poi matched
+	 * @param name the name of the Poi
+	 * @return the Poi that represent the input name,
+	 * return null if no Poi matched
+	 */
+	public static Poi getByName(String name){
 		//TODO
 		return null;
 	}
