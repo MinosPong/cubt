7e1b29793cf9565e8e98bc71777aa88d68dc8a95
kalynsan <kalynsan@gmail.com>
2011-05-06 08:04:03 +0000

    Several bus in service is eligible.

--- src/edu/cuhk/cubt/ui/com/ServiceOverlay.java
+++ src/edu/cuhk/cubt/ui/com/ServiceOverlay.java
@@ -1,6 +1,8 @@
 package edu.cuhk.cubt.ui.com;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
 
 import android.app.AlertDialog;
 import android.content.Context;
@@ -19,18 +21,27 @@ public class ServiceOverlay extends ItemizedOverlay<OverlayItem> {
 	private ArrayList<OverlayItem> mOverlays = new ArrayList<OverlayItem>();
 	private Context mContext;
 	String routeName = RouteData.ROUTE_0;
+	String pRoute, dir, lStop;
 	
 	public ServiceOverlay(Drawable defaultMarker, Context context) {
 		super(boundCenterBottom(defaultMarker));
 		mContext = context;
+		updateDisplay();
 		// TODO Auto-generated constructor stub
 	}
 
-	public void addOverlay(OverlayItem overlay) {
-	    mOverlays.add(overlay);
-	    populate();
+	public void updateDisplay(){
+		mOverlays.clear();
+		Iterator<GeoPoint> it = busInService().iterator();
+		if(it != null){
+			while(it.hasNext()){
+			GeoPoint busLoc = it.next();
+			mOverlays.add(new OverlayItem(busLoc, "Last Stop", "Predicted Route:"+ pRoute + "\nDirection: " + dir + "\nLast Stop:"+ lStop));
+			}
+		}
+		populate();
 	}
-	
+
 	@Override
 	protected boolean onTap(int index) {
 	  OverlayItem item = mOverlays.get(index);
@@ -39,7 +50,7 @@ public class ServiceOverlay extends ItemizedOverlay<OverlayItem> {
 	  AlertDialog.Builder dialog = new AlertDialog.Builder(mContext);
 	  dialog.setTitle(item.getTitle());
 	  dialog.setMessage(item.getSnippet());
-	  //dialog.show();
+	  dialog.show();
 	  return true;
 	}
 	
@@ -56,4 +67,11 @@ public class ServiceOverlay extends ItemizedOverlay<OverlayItem> {
 		//return 0;
 	}
 
+	//Received bus locations
+	Collection<GeoPoint> busInService(){
+		Collection<GeoPoint> bus = new ArrayList<GeoPoint>();
+		bus.add(new GeoPoint((int)(22.41988*1e6),(int)(114.20551*1e6)));
+		bus.add(new GeoPoint((int)(22.41608*1e6),(int)(114.2088*1e6)));
+		return bus;
+	}
 }
