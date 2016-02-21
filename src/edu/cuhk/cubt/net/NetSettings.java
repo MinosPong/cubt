3b73676e0e576e82923ee61939b7340891865c42
Minos Pong <pongkaming@gmail.com>
2016-02-21 16:53:07 +0000

    Remove out-dated project layout

--- src/edu/cuhk/cubt/net/NetSettings.java
+++
@@ -1,7 +0,0 @@
-package edu.cuhk.cubt.net;
-
-public class NetSettings {
-
-	protected static final String URL = "http://137.189.97.174:8080/servlet/FYPServer";
-	protected static final String PASS_VALUE =  "ieakpuser";
-}
--- src/edu/cuhk/cubt/service/IServiceMonitor.java
+++
@@ -1,8 +0,0 @@
-package edu.cuhk.cubt.service;
-
-import edu.cuhk.cubt.ui.CubtService;
-
-public interface IServiceMonitor {
-	public void start(CubtService service);
-	public void stop(CubtService service);
-}
